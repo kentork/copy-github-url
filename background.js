a602be51fb80a2e5d3a8929cfa0a8c0d50587d64
kentork <4198504+kentork@users.noreply.github.com>
2017-12-05 16:34:00 +0900

    remove alert

--- background.js
+++ background.js
@@ -4,7 +4,6 @@ chrome.contextMenus.create({
   onclick: () => {
     chrome.tabs.getSelected(null, function (tab) {
       const url = tab.url.match(/https:\/\/github\.com\/[a-zA-z0-9_\.\-]+\/[a-zA-z0-9_\.\-]+/)
-      alert(url)
       let area = document.getElementById('clipboard')
       area.value = url[0]
       area.select()
